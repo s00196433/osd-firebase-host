2636d57da9d3c64a7a721d98059147e64d03f450
s00196433 <s00196433@mail.itsligo.ie>
2021-12-17 22:26:40 +0000

    http

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from 'src/app/user.service';
+
+
+
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    signinForm: FormGroup = new FormGroup({});
+    message: String = "";
+    
+    constructor(
+        private fb: FormBuilder,
+        private userService: UserService
+    ) {  }
 
-  ngOnInit(): void {
-  }
+    ngOnInit() {
+        this.signinForm = this.fb.group({
+            email: [null, [Validators.required, Validators.email]],
+            password: [null, Validators.required]
+        });
+    }
+    get form() 
+    { 
+        return this.signinForm.controls; 
+    }
 
+    onSubmit() {
+        this.userService.login(this.form.email.value, this.form.password.value)
+            .subscribe(
+                data => {
+                    console.log(data);
+                    this.message = `Login successful for ${data.firstName} need to implement redirect`
+                },
+                error => {
+                    console.log(error);
+                    this.message = "Unable to log in please try again"
+                });
+    }
+
+    dismissAlert() {
+        this.message = "";
+    }
 }
